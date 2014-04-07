6a2856305da2d5ec008a16afdd1be24ff7bf9e16
Marcus Hammarberg <marcusoft.net@gmail.com>
2014-04-07 21:54:37 +0700

    First failing validation test

--- routes/motive.js
+++ routes/motive.js
@@ -4,7 +4,28 @@ var fs = require('fs');
 
 // Handlers
 function *addMotive(){
-	this.body = "addMotive";
+	var parts = parse(this);
+
+	var part;
+	var motive = {};
+	while (part = yield parts) {
+		// Busboy parts are arrays or streams
+		// field values are in arrays
+		if(part.length){
+			if(part[0] === "motiveName"){
+				motive.name = part[1];
+			}
+		}
+		else {
+			var stream = fs.createWriteStream('/tmp/' + Math.random() + part.filename);
+			part.pipe(stream);
+			console.log('uploading %s -> %s', part.filename, stream.path);
+		}
+	}
+
+	console.log("Just uploaded a motive for " + motive.name);
+
+	this.status = 201;
 };
 
 module.exports.addMotive = addMotive;
--- test/newMotive.specs.js
+++ test/newMotive.specs.js
@@ -3,12 +3,25 @@ var should = require('should');
 var request = require('supertest').agent(app.listen());
 
 describe('Creating new motives', function () {
+	var NEW_MOTIVE_URL = '/api/motive/new';
+	var TESTPIC_PATH = __dirname + '/fixtures/testpic.gif';
+
 	it('works fine for correct indata', function (done) {
 		request
-			.post()
-.post('/')
-.attach('avatar', 'test/fixtures/homeboy.jpg')
+			.post(NEW_MOTIVE_URL)
+			.field('motiveName', 'anUniqueName')
+			.attach('motiveFile', TESTPIC_PATH)
+			.expect(201)
+			.end(done);
 	});
-	it('requires a motive name and a file');
+	it('requires a motive name', function (done) {
+		request
+			.post(NEW_MOTIVE_URL)
+			.attach('motiveFile', TESTPIC_PATH)
+			.expect(400)
+			.expect({ message: 'Motive name is required' })
+			.end(done);
+	});
+	it('requires a motive file');
 	it('the motive name needs to be unique in the database');
 });
